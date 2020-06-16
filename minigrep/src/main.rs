dbaa30b3634bb30eba0d235c02d063a34f0d92dc
kalindimitrovv <kalindimitrovv@gmail.com>
2020-06-17 00:12:28 +0300

    small refactoring of minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,10 +4,10 @@ use libminigrep::ProgramArguments;
 
 fn main() {
     //v.iter().map(AsRef::as_ref).collect();
-    let args : Vec<String> = env::args().collect();
-    let args: Vec<&str> = args.iter().map(|x| &**x).collect();
+    // let args : Vec<String> = env::args().collect();
+    // let args: Vec<&str> = args.iter().map(|x| &**x).collect();
 
-    let parsed_result = ProgramArguments::new(&args[..]);
+    let parsed_result = ProgramArguments::new(env::args());
 
     let _result = parsed_result.unwrap_or_else(|e| {
         eprintln!("Problem parsing arguments: {}", e);
